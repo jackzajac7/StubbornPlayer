119e4f0af306d7dc31d2712752b0d1d614560c9f
jackzajac7 <azajac1@mail.csuchico.edu>
2018-03-05 14:41:21 -0800

    SecretDoor

--- main.cpp
+++ main.cpp
@@ -1,11 +1,31 @@
+//Aero Zajac
 #include "SecretDoor/SecretDoor.h"
 #include <iostream>
-
-using namespace std;
 
 int main()
 {
   SecretDoor game;
+  int repeat=0;
+  int dubs=0;
+
+  std::cout<< "How many times would you like to play?" <<std::endl;
+  std::cin>>repeat;
+  std::cout<< "Let's play!" <<std::endl;
+
+  while(repeat > 0) {
+
+  	game.newGame();
+  	game.guessDoorC();
+  	game.guessDoorC();
+  	if(game.isWinner() == true) {
+
+  		dubs++;
+
+  	}
+
+  }
+
+  std::cout<< "You only won "<<dubs<<", it's time to give up!" <<std::endl;
     
   return 0;
 }
